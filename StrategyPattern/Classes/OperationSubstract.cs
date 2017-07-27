550017815586146d5f18ee5c55290cf2febf8986
mtachot <matthieu.tachot@hotmail.fr>
2017-07-27 09:17:50 +0200

    First commit

---
+++ StrategyPattern/Classes/OperationSubstract.cs
@@ -0,0 +1,17 @@
+﻿using StrategyPattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern.Classes
+{
+    public class OperationSubstract : IStrategy
+    {
+        public int DoOperation(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+    }
+}
---
+++ StrategyPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using StrategyPattern.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Context context = new Context(new OperationAdd());
+            Console.WriteLine(String.Format("5 + 10 = {0}", context.ExecuteStrategy(5, 10)));
+
+            context = new Context(new OperationSubstract());
+            Console.WriteLine(String.Format("5 - 10 = {0}", context.ExecuteStrategy(5, 10)));
+
+            context = new Context(new OperationMultiply());
+            Console.WriteLine(String.Format("5 * 10 = {0}", context.ExecuteStrategy(5, 10)));
+
+            Console.ReadLine();
+        }
+    }
+}
